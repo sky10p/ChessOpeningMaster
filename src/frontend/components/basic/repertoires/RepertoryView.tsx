8e62a9e1b4f4c8c3fd39647161254f67fbe3a6c9
Pablo Guijarro <sky10pg@gmail.com>
2023-04-16 10:58:38 +0200

    feat: finished board

--- src/frontend/components/basic/repertoires/RepertoryView.tsx
+++ src/frontend/components/basic/repertoires/RepertoryView.tsx
@@ -1,28 +1,127 @@
 // src/components/RepertoryView.tsx
-import React, { useState, useEffect } from 'react';
-import { Chess } from 'chess.js'
-import Chessboard from 'chessboardjsx';
+import React, { CSSProperties, useEffect, useState } from "react";
+import { Chess, Color, Move, Square } from "chess.js";
+import Chessboard from "chessboardjsx";
 
 const RepertoryView = () => {
   const [chess] = useState(new Chess());
   const [fen, setFen] = useState(chess.fen());
+  const [squareStyles, setSquareStyles] = useState({});
+  const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
+  const [possibleMoves, setPossibleMoves] = useState<Move[]>([]);
+  const [dragOverSquare, setDragOverSquare] = useState<Square | null>(null);
+
+  const dropSquareStyle = {
+    background: "rgba(20, 85, 30, 0.4)",
+  };
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      const moves = chess.moves();
-      if (moves.length > 0) {
-        const randomMove = moves[Math.floor(Math.random() * moves.length)];
-        chess.move(randomMove);
+    const styles: Record<string, CSSProperties> = {};
+
+    if (selectedSquare) {
+      styles[selectedSquare] = {
+        background: "rgba(20, 85, 30, 0.5)",
+      };
+    }
+
+    for (const move of possibleMoves) {
+      styles[move.to] = {
+        background:
+          "radial-gradient(rgba(20, 85, 30, 0.5) 19%, rgba(0, 0, 0, 0) 20%)",
+      };
+    }
+
+    if (dragOverSquare) {
+      styles[dragOverSquare] = dropSquareStyle;
+    }
+
+
+    setSquareStyles(styles);
+  }, [selectedSquare, possibleMoves, dragOverSquare]);
+
+  const isCorrectPieceSelected = (square: Square, turn: Color) => {
+    const piece = chess.get(square);
+
+    return piece && piece.color === turn;
+  };
+
+  const selectPiece = (square: Square) => {
+    if (isCorrectPieceSelected(square, chess.turn())) {
+      setSelectedSquare(square);
+      const moves = chess.moves({ square: square as Square, verbose: true });
+      setPossibleMoves(moves);
+    } else {
+      unselectPiece();
+    }
+  };
+
+  const unselectPiece = () => {
+    setSelectedSquare(null);
+    setSquareStyles({});
+    setPossibleMoves([]);
+  };
+
+  const isMoveValid = (from: string, to: string): boolean => {
+    return possibleMoves.some((move) => move.from === from && move.to === to);
+  };
+
+  const handleMove = (from: string, to: string) => {
+    if (isMoveValid(from, to)) {
+      const move = chess.move({ from, to, promotion: "q" });
+      if (move) {
         setFen(chess.fen());
       }
-    }, 1000);
+    }
+    setPossibleMoves([]);
+    setSelectedSquare(null);
+    setSquareStyles({});
+  };
 
-    return () => clearInterval(timer);
-  }, [chess]);
+
+  const handleSquareClick = (square: Square) => {
+    if (!selectedSquare) {
+      selectPiece(square);
+    } else if (selectedSquare === square) {
+      unselectPiece();
+    } else if (selectedSquare && !isMoveValid(selectedSquare, square)) {
+      selectPiece(square);
+    } else if (selectedSquare && isMoveValid(selectedSquare, square)) {
+      handleMove(selectedSquare, square);
+    }
+  };
+
+  const onDrop = ({
+    sourceSquare,
+    targetSquare,
+  }: {
+    sourceSquare: string;
+    targetSquare: string;
+  }) => {
+    handleMove(sourceSquare, targetSquare);
+  };
+
+  const onDragOverSquare = (square: Square) => {
+    if (!selectedSquare) {
+      selectPiece(square);
+    }
+
+    if (possibleMoves.some((move) => move.to === square)) {
+      setDragOverSquare(square);
+    } else {
+      setDragOverSquare(null);
+    }
+  };
 
   return (
     <div>
-      <Chessboard position={fen} />
+      <Chessboard
+        position={fen}
+        onSquareClick={handleSquareClick}
+        onDrop={onDrop}
+        onDragOverSquare={onDragOverSquare}
+        squareStyles={squareStyles}
+        dropSquareStyle={{}}
+      />
     </div>
   );
 };
