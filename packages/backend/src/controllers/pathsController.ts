38def8bab01c6283d098d4c8e5bb99e6bfb20565
Pablo Guijarro <sky10pg@gmail.com>
2025-05-06 00:20:35 +0200

    refactor: Extract variant ID extraction logic into a separate function

--- packages/backend/src/controllers/pathsController.ts
+++ packages/backend/src/controllers/pathsController.ts
@@ -5,6 +5,14 @@ import { StudySession } from "../models/Study";
 import { Path } from "../models/Path";
 import { Db, ObjectId } from "mongodb";
 
+type VariantIdType = { $oid: string } | string | ObjectId;
+
+const extractVariantId = (variantId: VariantIdType): string => {
+  return typeof variantId === 'object' && '$oid' in variantId 
+    ? variantId.$oid 
+    : String(variantId);
+};
+
 const getRepertoireName = async (db: Db, repertoireId: string): Promise<string> => {
   const repertoire = await db.collection("repertoires").findOne({ _id: new ObjectId(repertoireId) });
   return repertoire ? repertoire.name : "Unknown Repertoire";
@@ -82,9 +90,7 @@ export async function getPaths(req: Request, res: Response, next: NextFunction) 
     let result: Path;
     if (variantToReview && (!studyToReview || variantToReview.errors > 0)) {
       
-      const variantId = typeof variantToReview._id === 'object' && '$oid' in variantToReview._id 
-        ? variantToReview._id.$oid 
-        : String(variantToReview._id);
+      const variantId = extractVariantId(variantToReview._id);
         
       result = {
         type: "variant",
